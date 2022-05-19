98f5ede8a11a25ef819888aebd2951b2cf2ea94f
manodecristiano <cristian.carre.online@gmail.com>
2022-05-20 01:21:20 +0200

    commit init (Todo Funciona)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
+import logo from './img/bubble.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo} className="w-3 h-5" alt="logo" />
+        <p className='text-green-300'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
