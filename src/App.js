d2c52c89032e9be756a3229e1f772cc1b3c8e3ac
manodecristiano <cristian.carre.online@gmail.com>
2022-05-20 10:17:44 +0200

    Create first Component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 
 function App() {
   return (
+    
   <div className="flex max-w-sm my-0 ms-auto p-6 rounded-lg bg-white shadow-xl">
-    <div class="chat-notification-logo-wrapper">
+    <div class="flex-shrink-0 "> 
      <img src={logo} className="w-14 h-14" alt="logo" />
     </div>
-  <div class="chat-notification-content">
-    <h4 class="chat-notification-title">ChitChat</h4>
-    <p class="chat-notification-message">You have a new message!</p>
+  <div class="ml-6 pt-l">
+    <h4 class="text-gray-900 text-xl">ChitChat</h4>
+    <p class="text-gray-500 text-base">You have a new message!</p>
   </div>
 </div>
+
   );
 }
 
